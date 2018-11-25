af6e26fab147762605279b2ed33d34c6639ad85c
unknown <lennickk1@gmail.com>
2018-11-24 19:06:55 -0600

    Project is created. Tables need references

---
+++ MovieApp/MovieApp/Models/Blog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Models
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public ICollection<Post> Posts { get; set; }
+    }
+}
